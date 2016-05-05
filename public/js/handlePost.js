c530cadb84eb96a8695237ba51a0c759b9bcaac6
Quiin <carlos.reyna21@gmail.com>
2016-05-05 15:10:45 -0500

    redirect to /dead

--- public/js/handlePost.js
+++ public/js/handlePost.js
@@ -2,7 +2,14 @@ $('#console-input').submit(function(event){
 	event.preventDefault();
 	$.post("/", $('#console-input').serialize(), function(result) {
 		var res = JSON.parse(result);		
+		var desc = res.desc
+		var player = res.player
+		var status = res.is_dead
   	$("#console-output").html(res.desc);
   	$('#input-command').val('');
+  	// $('#health').append('send help');
+  	if(status){
+  		window.location.replace("/dead");
+  	}
 	});
 })
